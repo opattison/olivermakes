b7ba10863ed8a0241a0e2237a73c44073bc08218
Oliver Pattison <oliverpattison@gmail.com>
2016-01-28 10:01:34 -0500

    try out named module pattern configuration

--- resources/scripts/site.js
+++ resources/scripts/site.js
@@ -1,25 +1,27 @@
-/* select page markers */
-var Top = document.querySelector('#top');
-var backTop = document.querySelector('.return-action--top');
+var Scrolling = (function () {
+  /* select page markers */
+  var Top = document.querySelector('#top');
+  var backTop = document.querySelector('.return-action--top');
 
-/* use Velocity UI function to scroll to any destination */
-function scroll (destination) {
-  Velocity(
-    destination,
-    'scroll',
-    {
-      duration: 320,
-      easing: 'easeInOutExpo'
-    }
-  )
-};
+  /* use Velocity UI function to scroll to any destination */
+  function scroll (destination) {
+    Velocity(
+      destination,
+      'scroll',
+      {
+        duration: 320,
+        easing: 'easeInOutExpo'
+      }
+    )
+  };
 
-/* back-to-top function and click listener */
-function scrollTop (event) {
-  event.preventDefault();
-  scroll(Top);
-};
+  /* back-to-top function and click listener */
+  function scrollTop (event) {
+    event.preventDefault();
+    scroll(Top);
+  };
 
-if (backTop) {
-  backTop.addEventListener('click', scrollTop, false);
-}
+  if (backTop) {
+    backTop.addEventListener('click', scrollTop, false);
+  }
+})();
